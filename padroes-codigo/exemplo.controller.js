f40f485815e781e53d942b9589098a039575712a
kelvinpalves <kelvinpalves@gmail.com>
2016-12-09 11:55:21 -0200

    Padrões de código para o controller

--- padroes-codigo/exemplo.controller.js
+++ padroes-codigo/exemplo.controller.js
@@ -6,21 +6,25 @@
 		.module('app')
 		.controller('Exemplo', Exemplo);
 
+	// Injeção de dependência.
 	Exemplo.$inject = ['somar'];
 
 	function Exemplo (somar) {
 		/* jshint validthis: true */
 		var vm = this;
 
+		// Objetos e funções com bind no topo e ordenadas
+		vm.executarSoma = executarSoma;
 		vm.numero1;
 		vm.numero2;
 		vm.resultado = 0;
-		vm.executarSoma = executarSoma;
-				
+		// objeto sem o bind
 		var totalDeSomas = 0;
 
 		function executarSoma () {
+			//Removendo a lógica de soma e a transferindo para uma factory. (somar.factory.js)
 			vm.resultado = somar.executar(vm.numero1, vm.numero2);
+			//Função privada
 			totalizador();
 		}
 
